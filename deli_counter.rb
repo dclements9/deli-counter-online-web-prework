16658070e01da1bd403ef49ce3cb1db605e22c9e
dclements <dclements@protonmail.com>
2018-12-19 18:46:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,11 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    line_array = []
-    counter = 1
-    katz_deli.each {|name| line_array << "#{counter}. #{name} "}
-    counter += 1
-    puts "The line is currently:#{line_array}"
-  end
-end
+    current_line = "The line is currently:"
+     katz_deli.each_with_index(1) {|name, index| current_line << " #{index + 1}. #{name}"}
+     puts current_line
+   end
+ end
 
 def take_a_number(katz_deli, join_name)
   katz_deli.pop(join_name)
