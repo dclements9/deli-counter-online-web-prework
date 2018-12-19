25bfcb442805f4dcbcbff2b7899bb830c86a16df
dclements <dclements@protonmail.com>
2018-12-19 13:05:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 katz_deli = []
 
-def line(line_array)
-  if line_array.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{line_array}"
+    puts "The line is currently: #{katz_deli}"
   end
 end
