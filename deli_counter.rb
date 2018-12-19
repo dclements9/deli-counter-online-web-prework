148af0f11ecd58070a765fd678755c0c683676bd
dclements <dclements@protonmail.com>
2018-12-19 18:57:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,10 +17,11 @@ def take_a_number(deli_line, join_name)
 end
 
 def now_serving(deli_line)
-  puts deli_line
+  
   if deli_line.length == 0
     puts "There is nobody waiting to be served!"
   else
+    puts deli_line
     puts "Currently serving #{deli_line.shift}."
     binding.pry
   end
