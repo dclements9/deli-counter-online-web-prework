256ef1d7e011da5ba481e62b10aacaf414a578fa
dclements <dclements@protonmail.com>
2018-12-19 18:25:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,3 +7,9 @@ def line(katz_deli)
     puts "The line is currently: #{katz_deli}"
   end
 end
+
+def take_a_number(katz_deli, join_name)
+  katz_deli.pop(join_name)
+  puts "#{katz_deli.length}#{join_name}"
+
+end
